a65433e8ab1b20faad69490f4c5547eaf6720814
Andrew Schonfeld <aschonfeld@numeric.com>
2018-04-27 23:09:55 -0400

    added tests for FactorViewer

---
+++ static/__tests__/factor_viewer/FactorViewer-test.jsx
@@ -0,0 +1,113 @@
+import { mount } from "enzyme";
+import _ from "lodash";
+import proxyquire from "proxyquire";
+import React from "react";
+import { Provider } from "react-redux";
+
+import { JSAnchor } from "../../JSAnchor";
+import reduxUtils from "../redux-test-utils";
+import { test, withGlobalJquery } from "../test-utils";
+
+const { actions, FactorViewer, Factor, ReturnsChart } = withGlobalJquery(() => {
+  const { factorActions } = reduxUtils.buildLibs();
+  const Chart = (ctx, cfg) => {
+    const chartCfg = {
+      ctx,
+      cfg,
+      data: cfg.data,
+      destroyed: false,
+      getElementAtEvent: () => [{ _chart: { config: cfg }, _index: 0 }],
+    };
+    chartCfg.destroy = function destroy() {
+      chartCfg.destroyed = true;
+    };
+    return chartCfg;
+  };
+  const chartUtils = proxyquire("../../chartUtils", { "chart.js": Chart });
+  const tsChartUtils = proxyquire("../../tsChartUtils", { "./chartUtils": chartUtils });
+  const ReturnsChart = proxyquire("../../factor_viewer/ReturnsChart", { "../tsChartUtils": tsChartUtils });
+  const FactorInputs = proxyquire("../../factor_viewer/FactorInputs", { "../actions/factor-viewer": factorActions });
+  const Factor = proxyquire("../../factor_viewer/Factor", {
+    "../actions/factor-viewer": factorActions,
+    "../chartUtils": chartUtils,
+    "./FactorInputs": FactorInputs,
+    "./ReturnsChart": ReturnsChart,
+  });
+  const { FactorViewer } = proxyquire("../../factor_viewer/FactorViewer", {
+    "../actions/factor-viewer": factorActions,
+    "./Factor": Factor,
+  });
+  return { actions: factorActions, FactorViewer, Factor, ReturnsChart };
+});
+
+test("FactorViewer: rendering with redux", t => {
+  const store = reduxUtils.createFactorStore();
+  store.dispatch(actions.init());
+  setTimeout(() => {
+    const body = document.getElementsByTagName("body")[0];
+    body.innerHTML += '<div id="content"></div>';
+    const result = mount(
+      <Provider store={store}>
+        <FactorViewer />
+      </Provider>,
+      {
+        attachTo: document.getElementById("content"),
+      }
+    );
+
+    const options = result.find("ul.list-group");
+    const factors = options.find(JSAnchor).map(a =>
+      a
+        .find("div span")
+        .first()
+        .text()
+    );
+    t.deepEqual(_.map(_.range(1, 14), id => `Factor ${id}`), factors, "should render correct factors");
+
+    const factor = result.find(Factor.ReactFactor);
+    t.equal(factor.instance().props.selectedFactor, "factor_1", "should select first factor");
+
+    let returns = factor.find(ReturnsChart.ReactReturnsChart);
+    const activeReturn = returns
+      .find("div.return-types")
+      .find("button.active")
+      .text();
+    returns
+      .find("div.return-types")
+      .find("button.inactive")
+      .simulate("click");
+    result.update();
+    returns = result.find(Factor.ReactFactor).find(ReturnsChart.ReactReturnsChart);
+    t.equal(
+      activeReturn,
+      returns
+        .find("div.return-types")
+        .find("button.inactive")
+        .text(),
+      "should toggle return type"
+    );
+
+    const inactiveFactor = result
+      .find("ul.list-group")
+      .find("a.inactive")
+      .first();
+    inactiveFactor.simulate("click");
+    result.update();
+    setTimeout(() => {
+      result.update();
+      t.equal(
+        inactiveFactor
+          .find("div span")
+          .first()
+          .text(),
+        result
+          .find(Factor.ReactFactor)
+          .find("h2")
+          .first()
+          .text(),
+        "should toggle factor"
+      );
+      t.end();
+    }, 10);
+  }, 400);
+});
--- static/__tests__/redux-test-utils.jsx
+++ static/__tests__/redux-test-utils.jsx
@@ -1,7 +1,9 @@
 /* eslint complexity: "off" */
 /* eslint max-statements: "off" */
 /* eslint max-lines: "off" */
+import qs from "querystring";
 
+import _ from "lodash";
 import proxyquire from "proxyquire";
 
 import factorApp from "../reducers/factor-viewer";
@@ -10,14 +12,28 @@ import { createStore } from "../reducers/store";
 import summaryApp from "../reducers/summary-viewer";
 import GicsMappingsData from "./GicsMappings-data";
 import mockPopsicle from "./MockPopsicle";
+import FactorData from "./factor_viewer/Factor-data";
+import FactorOptionsData from "./factor_viewer/FactorOptions-data";
 import CumulativeReturnsData from "./results_viewer/CumulativeReturns-data";
 import ResultsStatsData from "./results_viewer/ResultsStats-data";
 import SampleIndexesData from "./results_viewer/SampleIndexes-data";
 import UserResultsData from "./results_viewer/UserResults-data";
 
 function urlFetcher(url) {
+  const params = qs.parse(url.split("?")[1]);
   if (url.startsWith("/index-builder/gics-mappings")) {
     return GicsMappingsData;
+  }
+  if (url.startsWith("/index-builder/load-factor-settings")) {
+    return { factors: {}, locked: false };
+  }
+  if (url.startsWith("/index-builder/factor-options")) {
+    return FactorOptionsData;
+  }
+  if (url.startsWith("/index-builder/factor-data")) {
+    const label = `Factor ${_.last(params.factor.split("_"))}`;
+    const updateDetails = { description: `Description of "${label}"`, id: params.factor, label };
+    return _.assignIn({}, FactorData, updateDetails);
   }
   if (url.startsWith("/index-builder/results-stats")) {
     return ResultsStatsData;
